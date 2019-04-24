8f322f3a428d07c8458d9d72a24e6d7675f09910
sojaekyung <sojaekyung@ownerclan.com>
2019-04-24 13:36:38 +0900

    make type inference work better with functions

--- src/sqlFunction.ts
+++ src/sqlFunction.ts
@@ -1,30 +1,30 @@
-import { Expression } from "./expression";
+import { Expression, Variable } from "./expression";
 import { q } from "./index";
 
-export function count(x: Expression<unknown>): Expression<number> {
+export function count(x: Expression<unknown>): Variable<number> {
   return q`COUNT(${x})`;
 }
 
-export function sum(x: Expression<unknown>): Expression<number> {
+export function sum(x: Expression<unknown>): Variable<number> {
   return q`SUM(${x})`;
 }
 
-export function avg(x: Expression<unknown>): Expression<number> {
+export function avg(x: Expression<unknown>): Variable<number> {
   return q`AVG(${x})`;
 }
 
-export function distinct<T>(x: Expression<T>): Expression<T> {
+export function distinct<T>(x: Expression<T>): Variable<T> {
   return q`DISTINCT(${x})`;
 }
 
-export function between<T = 0 | 1>(x: Expression<T>, left: Expression<T>, right: Expression<T>): Expression<T> {
+export function between<T = 0 | 1>(x: Expression<unknown>, left: Expression<unknown>, right: Expression<unknown>): Variable<T> {
   return q`(${x}) BETWEEN (${left}) AND (${right})`;
 }
 
-export function and(left: Expression<unknown>, right: Expression<unknown>): Expression<0 | 1> {
+export function and(left: Expression<unknown>, right: Expression<unknown>): Variable<0 | 1> {
   return q`(${left}) AND (${right})`;
 }
 
-export function or(left: Expression<unknown>, right: Expression<unknown>): Expression<0 | 1> {
+export function or(left: Expression<unknown>, right: Expression<unknown>): Variable<0 | 1> {
   return q`(${left}) OR (${right})`;
 }
